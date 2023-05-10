f57e562698c4b38e3d03afe9127ae1bb97181aa8
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-10 17:19:46 +0530

    added user test model

---
+++ src/model/user_model.ts
@@ -0,0 +1,16 @@
+import { Document, Schema, model } from 'mongoose';
+
+// User interface
+interface IUser extends Document {
+  email: string;
+  password: string;
+}
+
+// User schema
+const userSchema = new Schema<IUser>({
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+// User model
+const User = model<IUser>('User', userSchema);
