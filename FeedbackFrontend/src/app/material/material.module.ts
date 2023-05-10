c8dd3aff3eb17d1f2af4fb5bcdbc1500310590a0
Siddharth Garg <siddharth.g@codestoresolutions.com>
2023-05-10 11:40:52 +0530

    Feedback Management - Modular Project Setup
    Initial setup for the feedback management based on module design pattern.

---
+++ FeedbackFrontend/src/app/material/material.module.ts
@@ -0,0 +1,81 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MatRippleModule } from '@angular/material/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MatRippleModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatTooltipModule
+  ],
+  exports: [
+    MatRippleModule,
+    MatToolbarModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatBadgeModule,
+    MatCardModule,
+    MatDialogModule,
+    MatFormFieldModule,
+    MatExpansionModule,
+    MatDividerModule,
+    MatIconModule,
+    MatListModule,
+    MatMenuModule,
+    MatPaginatorModule,
+    MatProgressSpinnerModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatTreeModule,
+    MatTooltipModule
+  ]
+})
+export class MaterialModule { }
