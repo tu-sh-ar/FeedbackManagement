21ee4ee07f2e95a1ed09aa47628eb9a68d0f77e1
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-10 17:26:37 +0530

    added client test model

---
+++ src/model/client_model.ts
@@ -0,0 +1,16 @@
+import { Document, Schema, model } from 'mongoose';
+
+interface IClient extends Document {
+    email: string;
+    password: string;
+  }
+  
+  // Client schema
+  const clientSchema = new Schema<IClient>({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  });
+  
+  // Client model
+  const Client = model<IClient>('Client', clientSchema);
+  
