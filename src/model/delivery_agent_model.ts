5654e18f5a3522b16582401fd15e0b3aa82d5430
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-10 17:38:44 +0530

    updated delivery agent test model

--- src/model/delivery_agent_model.ts
+++ src/model/delivery_agent_model.ts
@@ -3,18 +3,18 @@ import { Document, Schema, model } from 'mongoose';
 interface IDeliveryAgent extends Document {
     email: string;
     password: string;
-    client: Schema.Types.ObjectId;
-    user: Schema.Types.ObjectId;
-    product: Schema.Types.ObjectId;
+    client_id: Schema.Types.ObjectId;
+    user_id: Schema.Types.ObjectId;
+    product_id: Schema.Types.ObjectId;
   }
   
   // Delivery Agent schema
   const deliveryAgentSchema = new Schema<IDeliveryAgent>({
     email: { type: String, required: true },
     password: { type: String, required: true },
-    client: { type: Schema.Types.ObjectId, ref: 'Client', required: true },
-    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
-    product: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
+    client_id: { type: Schema.Types.ObjectId, ref: 'Client', required: true },
+    user_id: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    product_id: { type: Schema.Types.ObjectId, ref: 'Product', required: true },
   });
   
   // Delivery Agent model
