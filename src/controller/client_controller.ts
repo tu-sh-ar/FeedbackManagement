245bd168c3c5fb48e8676db964994f44f8720408
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-11 16:04:50 +0530

    test client controller and authorisation

---
+++ src/controller/client_controller.ts
@@ -0,0 +1,27 @@
+import Client from '../model/client_model'
+import { Request, Response } from 'express'
+import jwt from 'jsonwebtoken'
+
+// create user 
+export const userSignup = async( req:Request, res:Response )  => {
+    const user_details = req.body;
+    await Client.create(user_details)
+    .then(user => res.status(201).send(user))
+    .catch(err => res.status(400).send(err))
+}
+
+// user login
+export const user_login = async(req:Request, res:Response) => {
+    const { email, password } = req.body;
+    await Client.findOne({email:email})
+    .then(data => {
+        const token = jwt.sign({
+            user:{
+                email:data?.email,
+                id:data?._id
+            }
+        },'secret',{expiresIn:"30m"});
+        res.status(200).send(token)
+    })
+    .catch(err => res.status(404).send("no user found"))
+}
--- src/middlewares/auth/jwt_auth.ts
+++ src/middlewares/auth/jwt_auth.ts
@@ -1,1 +1,25 @@
 // adding jwt verification for user and client 
+import jwt from 'jsonwebtoken'
+import { Request, Response, NextFunction } from 'express'
+
+export const verifytoken = async(req:Request, res:Response, next:NextFunction) => {
+    let token:string;
+    let authHeader = req.headers.Authorization || req.headers.authorization;
+
+    if(authHeader && authHeader.toString().startsWith('Bearer')) {
+        // in the authorisation section the index 0 will contain the bearer and the index 1 contain the accesstoken generated on user login 
+
+        token = authHeader.toString().split(" ")[1];            // converting the array to string 
+
+        const decoded = jwt.verify(token, 'secret');  // verifying the generated token using verify
+
+        if(decoded) {
+            // parsing the data 
+            const data = JSON.parse(JSON.stringify(decoded));
+            console.log(data);
+            next();
+        }
+
+        res.status(401).send("Invalid Token")
+    }
+}
