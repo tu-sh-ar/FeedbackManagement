5d0db743c3312a68af168dae99922e0694cc0385
Siddharth Garg <siddharth.g@codestoresolutions.com>
2023-05-12 16:04:35 +0530

    Feedback list pagination, sorting and filter.

--- FeedbackFrontend/src/app/feedback/feedback-list/feedback-list.component.ts
+++ FeedbackFrontend/src/app/feedback/feedback-list/feedback-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Feedback } from 'src/app/interfaces/feedback';
 import { FeedbackService } from 'src/app/services/feedback.service';
@@ -11,9 +12,10 @@ import { FeedbackService } from 'src/app/services/feedback.service';
 })
 export class FeedbackListComponent implements OnInit{
   @ViewChild('paginator') paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
   feedbacks!: Feedback[];
   dataSource!: MatTableDataSource<Feedback>;
-  displayFeedbackColumns: string[] = ['id', 'user', 'product', 'rating', 'created_at', 'updated_at'];
+  displayFeedbackColumns: string[] = ['id', 'user', 'product', 'rating', 'created_at', 'updated_at', 'review'];
 
   constructor(private _feedbackService: FeedbackService){}
 
@@ -23,6 +25,14 @@ export class FeedbackListComponent implements OnInit{
       console.log(this.feedbacks, " - feedbacks");
       this.dataSource = new MatTableDataSource(this.feedbacks);
       this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     })
   }
+
+  doFilter(event:Event){
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  openFeedbackDetailDialogue(){}
 }
