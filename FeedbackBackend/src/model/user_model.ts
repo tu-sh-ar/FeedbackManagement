a5034acc8db881d11dbeda17e845481790c00ff0
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-16 13:06:14 +0530

    removed version key

--- FeedbackBackend/src/model/user_model.ts
+++ FeedbackBackend/src/model/user_model.ts
@@ -2,12 +2,14 @@ import { Document, Schema, model } from 'mongoose';
 
 // User interface
 interface IUser extends Document {
+  name: string;
   email: string;
   password: string;
 }
 
 // User schema
 const userSchema = new Schema<IUser>({
+  name: { type: String, required:true},
   email: { type: String, required: true },
   password: { type: String, required: true },
   },
