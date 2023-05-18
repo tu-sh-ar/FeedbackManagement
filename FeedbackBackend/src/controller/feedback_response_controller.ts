d8bb3f56658610bb912132ff8495356b7b5d805e
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-18 16:44:25 +0530

    added delete response controller

--- FeedbackBackend/src/controller/feedback_response_controller.ts
+++ FeedbackBackend/src/controller/feedback_response_controller.ts
@@ -9,7 +9,7 @@ export const create_Response = async( req:Request, res:Response ) => {
 
     try {
         // saving the feeedback
-        FeedbackResponse.create(req.body)
+        await FeedbackResponse.create(req.body)
         .then(data => res.status(201).send(data))
         .catch(err => res.status(401).send("Response not Saved"))
 
@@ -20,16 +20,34 @@ export const create_Response = async( req:Request, res:Response ) => {
     }
 }
 
-export const update_response = async(req:Request, res:Response) => {
-    const updated_response = req.body;
-    const response_id = req.params.id;
+// // update response 
+// export const update_response = async(req:Request, res:Response) => {
+//     const updated_response = req.body;
+//     const response_id = req.params.id;
+
+//     try {
+//         const updated_data = await FeedbackResponse.findByIdAndUpdate(response_id, update_response);
+//         if(Object.keys(updated_data).length != 0){
+//             res.status(201).send(updated_data)
+//         }
+//     } catch (error) {
+//         res.status(500).json({error: `Internal Server Error ${error}`})
+//     }
+// }
+
+// delete response 
+export const delete_response = async(req:Request, res:Response) => {
+    const feedback_id = req.params.id;
 
     try {
-        const updated_data = FeedbackResponse.findByIdAndUpdate(response_id, update_response);
-        if(Object.keys(updated_data).length != 0){
-            res.status(201).send(updated_data)
-        }
+
+         FeedbackResponse.findByIdAndDelete(feedback_id)
+        .then(data => res.status(200).send("deleted Succeessfully"))
+        .catch(err => res.status(400).json({error:"bad Request"}))
+
     } catch (error) {
-        res.status(500).json({error: `Internal Server Error ${error}`})
+
+        res.status(500).json({error:`internal server error: ${error}`})
+        
     }
 }
--- FeedbackBackend/src/routes/feedback_response_route.ts
+++ FeedbackBackend/src/routes/feedback_response_route.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router();
 
-import { create_Response, update_response } from '../controller/feedback_response_controller'
+import { create_Response, delete_response } from '../controller/feedback_response_controller'
 import { verifyToken } from '../middlewares/auth/jwt_auth';
 
 //router.use(verifyToken);
@@ -12,6 +12,6 @@ router.post(
     create_Response
    )
 
-router.put("/:id", update_response);
+router.delete("/:id", delete_response);
 
 export default router
