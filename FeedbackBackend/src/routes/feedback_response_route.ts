7164cf05984781ec5217440e39dee55d3bb277fc
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-18 13:13:00 +0530

    updated response route

--- FeedbackBackend/src/routes/feedback_response_route.ts
+++ FeedbackBackend/src/routes/feedback_response_route.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router();
 
-import { create_Response } from '../controller/feedback_response_controller'
+import { create_Response, update_response } from '../controller/feedback_response_controller'
 import { verifyToken } from '../middlewares/auth/jwt_auth';
 
 //router.use(verifyToken);
@@ -11,5 +11,7 @@ router.post(
    // ResponseNotification , 
     create_Response
    )
+
+router.put("/:id", update_response);
 
 export default router
