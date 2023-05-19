37a27366f74e311e55571ddaadf991921a556c22
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-19 16:55:08 +0530

    updated product controller

--- FeedbackBackend/src/controller/product_controller.ts
+++ FeedbackBackend/src/controller/product_controller.ts
@@ -1,14 +1,35 @@
 import Product from '../model/product_model'
 import { Request, Response } from 'express'
+import FeedbackModel from '../model/feedback_model';
 
 export const getProduct = async(req:Request, res:Response) => {
+
     const product_id = req.params.id;
+    let average_rating:any;
 
-    await Product.findById(product_id)
-    .then(data => res.status(200).send(data))
-    .catch(err => res.status(404).send(err))
+    try {
+
+        const feedback_data = await FeedbackModel.find({product_id:product_id})
+        
+        const ratings = feedback_data.map((feedback) => feedback.rating);
+        const sum = ratings.reduce((acc, rating) => acc + rating, 0);
+        average_rating = sum/ratings.length;
+        
+        const product_data = await Product.findById(product_id);
+        if(!product_data){
+            res.status(404).json({error:"No product found"})
+            return;
+        }
+
+        const updated_data = {...product_data.toObject() , avg_rating:average_rating}
+        res.status(200).send(updated_data);
+
+        
+    } catch (error) {
+        
+        res.status(500).send(error);
+    }
 }
-
 
 export const createProduct = async(req:Request, res:Response) => {
     const product_data = req.body;
@@ -17,3 +38,4 @@ export const createProduct = async(req:Request, res:Response) => {
     .then(data => res.status(201).send(data))
     .catch(err => res.status(400).send(err))
 }
+
