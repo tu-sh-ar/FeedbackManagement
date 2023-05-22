564ce6e08ec71973cf2def058c6779f2b80e49e6
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-22 12:50:06 +0530

    push notification using socket io

--- FeedbackBackend/src/middlewares/notification/response_notification.ts
+++ FeedbackBackend/src/middlewares/notification/response_notification.ts
@@ -1,1 +1,15 @@
 import { Server as SocketIOServer, Socket } from 'socket.io';
+
+const pushNotificationMiddleware = (io: SocketIOServer) => {
+    return (socket: Socket, next: (err?: Error) => void) => {
+      socket.on('response-generated', (data: { userId: string, message: string }) => {
+        const { userId, message } = data;
+        
+        // Emit the notification message to the specific user identified by userId
+        io.to(userId).emit('notification', { message });
+      });
+  
+      next();
+    };
+  };
+  
