1ff6db1a1bbd6d48a815a3903a0d0f1bde42d4ec
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-29 12:26:55 +0530

    added auto response api

--- FeedbackBackend/src/routes/feedback_response_route.ts
+++ FeedbackBackend/src/routes/feedback_response_route.ts
@@ -1,7 +1,7 @@
 import express from 'express'
 const router = express.Router();
 
-import { create_Response, deleteResponse, getResponse } from '../controller/feedback_response_controller'
+import { create_Response, deleteResponse, getResponse, createMissingResponse } from '../controller/feedback_response_controller'
 import { verifyToken } from '../middlewares/auth/jwt_auth';
 
 //router.use(verifyToken);
@@ -13,6 +13,8 @@ router.post(
     create_Response
    )
 
+router.post("/createAutoResponse", createMissingResponse);
 router.delete("/:id", deleteResponse);
+
 
 export default router
