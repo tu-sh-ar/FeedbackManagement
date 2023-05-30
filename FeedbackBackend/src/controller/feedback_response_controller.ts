a6bd2c4695f6322d1c8b6e3e667c23de5a61f547
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-30 13:46:38 +0530

    updated auto response api

--- FeedbackBackend/src/controller/feedback_response_controller.ts
+++ FeedbackBackend/src/controller/feedback_response_controller.ts
@@ -73,32 +73,33 @@ export const deleteResponse = async (req: Request, res: Response) => {
   };
 
   // generate automatic  response 
-  export const createMissingResponse = async (req: Request, res: Response) => {
+  export const createMissingResponse = async () => {
     try {
       const feedbacks = await FeedbackModel.find();
   
       for (const feedback of feedbacks) {
         const response = await FeedbackResponse.findOne({ feedback_id: feedback._id });
-       
+  
         let generatedResponse: string;
         if (feedback.rating > 3) {
           generatedResponse = "Thank you for your feedback.";
         } else {
           generatedResponse = "Sorry for the inconvenience. We will look into the issue.";
         }
+        
         if (!response) {
           const sevenDaysAgo = new Date();
           sevenDaysAgo.setDate(sevenDaysAgo.getDate() - 7);
           
           if (JSON.stringify(feedback.createdAt) < JSON.stringify(sevenDaysAgo)) {
-             await FeedbackResponse.create({ feedback_id: feedback._id, response: generatedResponse });
+            await FeedbackResponse.create({ feedback_id: feedback._id, response: generatedResponse });
           }
         }
       }
-
-      res.status(200).json({ message: "Automatic responses generated successfully." });
+  
+      console.log("Automatic responses generated successfully.");
     } catch (error) {
-      res.status(500).json({ error: "Internal Server Error" });
+      console.error("Error in automatic response generation:", error);
     }
   };
-
+  
