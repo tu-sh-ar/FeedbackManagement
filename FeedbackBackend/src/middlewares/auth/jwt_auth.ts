dbbef2ecb66f1ef19f93841975a4f77d7b6e004f
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-05-31 11:27:55 +0530

    updated authorization and token verification

--- FeedbackBackend/src/middlewares/auth/jwt_auth.ts
+++ FeedbackBackend/src/middlewares/auth/jwt_auth.ts
@@ -10,11 +10,23 @@ export const verifyToken = async (req: Request, res: Response, next: NextFunctio
     token = authHeader.toString().split(' ')[1];
 
     try {
-      const decoded = jwt.verify(token, 'secret');
+      const decodedToken = jwt.verify(token, 'my-32-character-ultra-secure-and-ultra-long-secret') as {
+        [key: string]: string;
+      };
 
-      if (decoded) {
-        const data = JSON.parse(JSON.stringify(decoded));
-        req.user = data.user;
+      if (decodedToken) {
+        // Access the token claims
+        const nameIdentifier = decodedToken['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'];
+        const role = decodedToken['http://schemas.microsoft.com/ws/2008/06/identity/claims/role'];
+
+        // Storing the extracted information for later use or pass it to the next middleware
+        req.user = {
+          nameIdentifier,
+          role,
+        };
+
+        console.log(req.user);
+
         next();
       } else {
         res.status(401).send('Invalid Token');
