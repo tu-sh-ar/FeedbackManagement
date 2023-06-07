0fcb18be83d14dc12cdc744a1aca6ddcc8bd2b8a
siddharth-garg-cs <siddharth.g@codestoresolutions.com>
2023-06-07 11:02:56 +0530

    UX improvements.

--- FeedbackFrontend/src/app/material/material.module.ts
+++ FeedbackFrontend/src/app/material/material.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatRippleModule } from '@angular/material/core';
+import {MatChipsModule} from '@angular/material/chips';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
@@ -51,7 +52,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatTableModule,
     MatTabsModule,
     MatTreeModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatChipsModule
   ],
   exports: [
     MatRippleModule,
@@ -75,7 +77,8 @@ import { MatTooltipModule } from '@angular/material/tooltip';
     MatTableModule,
     MatTabsModule,
     MatTreeModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatChipsModule
   ]
 })
 export class MaterialModule { }
