04926dfeb6d598e12c5f886577b2c2467492fe50
Yash Raj <67647073+yashraj9410@users.noreply.github.com>
2023-06-13 12:55:32 +0530

    added logging options

---
+++ FeedbackBackend/src/middlewares/logging_function/get_current_date.ts
@@ -0,0 +1,8 @@
+// get current date for logging file creation
+function getCurrentDate() {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const day = String(today.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
