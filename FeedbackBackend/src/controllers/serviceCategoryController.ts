7d7133bf9e1246c7317b7f6d50d28bb82c0d81d0
rvikunwar <ravindra.kunwar@creware.asia>
2023-08-03 13:50:20 +0530

    fix: updated api

--- FeedbackBackend/src/controllers/serviceCategoryController.ts
+++ FeedbackBackend/src/controllers/serviceCategoryController.ts
@@ -29,7 +29,6 @@ export const getServiceCategories = async (req: Request, res: Response) => {
         const modifiedCategories = categories.map(category => {
             category.id = category._id;
             delete category._id;
-            delete category.businessCategoryId;
             return category;
         });
 
@@ -40,42 +39,3 @@ export const getServiceCategories = async (req: Request, res: Response) => {
     }
 }
 
-
-export const getActiveLinkForTemplate = async (req: Request, res: Response) => {
-    try {
-        const serviceId = req.params.serviceId;
-        const businessAdminId = req.params.businessAdminId;
-
-        const bodyData = req.body as LinkBodyDto;
-
-        await validateLinkBodySchema(bodyData);
-
-        const existingTemplate: any = await BusinessAdmin.findOne(
-            {
-                'templates.active': true,
-                businessAdminId,
-                templateServiceCategoryId: new Types.ObjectId(serviceId),
-            },
-            { _id: 0, 'templates.$': 1 }
-        )
-
-        if (!existingTemplate) {
-            return buildErrorResponse(res, 'Template is not active', 404);
-        }
-
-        const templateObj = existingTemplate.templates[0]?.id;
-
-        const link = generateUrlWithToken(templateObj, bodyData);
-
-        return buildObjectResponse(res, link)
-
-    } catch (error) {
-        console.log(error)
-        if (error instanceof yup.ValidationError && error?.errors) {
-            const errorMessage: string = error.errors?.join(', ') || 'Validation Error';
-            return buildErrorResponse(res, errorMessage, 400);
-        } else {
-            return buildErrorResponse(res, 'Internal server error', 500);
-        }
-    }
-}
